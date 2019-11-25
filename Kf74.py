50d63eff9f262b195f9bf2daee2ea376b7c11b3c
techgym <tadashifujinaga@gmail.com>
2019-11-25 21:34:32 +0900

    Update Kf74.py

--- Kf74.py
+++ Kf74.py
@@ -1,2 +1,19 @@
-﻿#AI-TECHGYM-N-D
+#AI-TECHGYM-N-D
 import numpy as np
+
+#表示のためのライブラリの読み込み
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+#乱数
+import numpy.random as random
+
+#設定値
+init_seed = 0
+x_num = 100
+y_num = 100
+x_fig = 10
+y_fig = 10
+
+#シード値の設定
+random.seed(init_seed)
