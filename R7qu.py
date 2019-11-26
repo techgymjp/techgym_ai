2e66537df71e597a26c373c37d6d4e10edc6d9cc
techgym <tadashifujinaga@gmail.com>
2019-11-26 12:59:08 +0900

    Update R7qu.py

--- R7qu.py
+++ R7qu.py
@@ -1,2 +1,21 @@
-﻿#AI-TECHGYM-N-1
+#AI-TECHGYM-N-1
+
+#pandasライブラリの読み込み
 import pandas as pd
+
+hand_df = pd.Series(['グー','チョキ','パー'])
+
+#データフレームの表示
+
+
+#チョキのみを表示
+
+
+#データ型の表示
+
+
+#indexの変更
+
+
+
+#indexの表示
