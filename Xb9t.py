7eacdff539f78953e462ae23bdb5e7f8a258c348
techgym <tadashifujinaga@gmail.com>
2019-11-26 12:52:58 +0900

    Update Xb9t.py

--- Xb9t.py
+++ Xb9t.py
@@ -1,3 +1,29 @@
-﻿#AI-TECHGYM-N-E
+#AI-TECHGYM-N-E
 import numpy as np
+
+#表示のためのライブラリの読み込み
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+#ヘルプの表示
+#help(plt.hist)
+
+#乱数
+import numpy.random as random
+
+#設定値
+init_seed = 0
+hist_bin = 40
+
+# シードの固定
+
+
+# グラフの大きさ
+
+
+#プロットデータ
+data =
+
+# ヒストグラム
+
 
