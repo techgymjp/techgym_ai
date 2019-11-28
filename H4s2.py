fd0e10a328a1739f966a432321e5672606bba3b7
techgym <tadashifujinaga@gmail.com>
2019-11-28 20:03:52 +0900

    Update H4s2.py

--- H4s2.py
+++ H4s2.py
@@ -4,3 +4,22 @@ import scipy.linalg as linalg
 
 matrix = np.array([[1,-1,-1], [-1,1,-1], [-1,-1,1]])
 display(matrix)
+
+# 行列式
+display('行列式')
+
+
+# 逆行列
+display('逆行列')
+
+
+
+
+# 固有値と固有ベクトル
+eig_value, eig_vector = 
+
+# 固有値と固有ベクトル
+display('固有値')
+display(eig_value)
+display('固有ベクトル')
+display(eig_vector)
