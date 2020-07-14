80d98fcad196f5f7fd0f5821083f22f477e7625b
keiichisuga <keing312@gmail.com>
2020-07-14 19:57:44 +0900

    Update Yz2E.py

--- Yz2E.py
+++ Yz2E.py
@@ -24,15 +24,15 @@ sc.fit(X_train)
 X_train_std = sc.transform(X_train)
 X_test_std = sc.transform(X_test)
 
-model = LogisticRegression(random_state=0,solver='liblinear')
-model.fit(X_train_std,y_train)
+model_std = LogisticRegression(random_state=0,solver='liblinear')
+model_std.fit(X_train_std,y_train)
 
 print("標準化後")
-print('正解率(train):{:.3f}'.format(model.score(X_train_std, y_train)))
-print('正解率(test):{:.3f}'.format(model.score(X_test_std, y_test)))
+print('正解率(train):{:.3f}'.format(model_std.score(X_train_std, y_train)))
+print('正解率(test):{:.3f}'.format(model_std.score(X_test_std, y_test)))
 
-score_up_train = model.score(X_train_std, y_train) - model.score(X_train, y_train)
-score_up_test = model.score(X_test_std, y_test) - model.score(X_test, y_test)
+score_up_train = model_std.score(X_train_std, y_train) - model.score(X_train, y_train)
+score_up_test = model_std.score(X_test_std, y_test) - model.score(X_test, y_test)
 
 print("差分")
 print('正解率(train):{:.3f}'.format(score_up_train))
