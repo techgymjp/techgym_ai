646247deb9f150ecdf5fd3e42d1a4b896461d120
techgym <tadashifujinaga@gmail.com>
2023-11-30 21:33:16 +0900

    Update AI-TECHGYM-12-9-A.py
    brest_cancer.pklの作成コードはQフォルダにあります。

--- AI-OPEN-12/A/AI-TECHGYM-12-9-A.py
+++ AI-OPEN-12/A/AI-TECHGYM-12-9-A.py
@@ -5,7 +5,7 @@ import joblib
 
 fl_main = Flask(__name__)
 
-# 訓練済みモデルの読み込み
+# 訓練済みモデルの読み込み # brest_cancer.pklは、brest_cancer.pyで作成できます。
 clf = joblib.load('brest_cancer.pkl')
 
 @fl_main.route('/', methods=['POST'])
